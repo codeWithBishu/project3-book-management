17164592e7433dd1f3caf3b1ee86a9cbf0df9969
codeWithBishu <bisu311297@gmail.com>
2022-10-29 18:54:03 +0530

    done

---
+++ src/controllers/aws.js
@@ -0,0 +1,52 @@
+
+// const aws = require('aws-sdk')
+
+// aws.config.update({
+//     accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+//     secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+//     region: "ap-south-1"
+// })
+
+// let uploadFile= async ( file) =>{
+//    return new Promise( function(resolve, reject) {
+//     let s3= new aws.S3({apiVersion: '2006-03-01'}); 
+
+//     var uploadParams= {
+//         ACL: "public-read",
+//         Bucket: "classroom-training-bucket",  
+//         Key: "newFile/" + file.originalname, 
+//         Body: file.buffer
+//     }
+
+
+//     s3.upload( uploadParams, function (err, data ){
+//         if(err) {
+//             return reject({"error": err})
+//         }
+//         console.log(data)
+//         console.log("file uploaded succesfully")
+//         return resolve(data.Location)
+//     })
+//    })
+// }
+
+
+// const bookCover =  async function(req, res){
+
+//     try{
+//         let files= req.files
+//         if(files && files.length>0){
+//             let uploadedFileURL= await uploadFile( files[0] )
+//             res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+//         }
+//         else{
+//             res.status(400).send({ msg: "No file found" })
+//         }
+        
+//     }
+//     catch(err){
+//         res.status(500).send({msg: err})
+//     }
+    
+// }
+// module.exports.bookCover = bookCover;
